799ae647e63422496276858c85150341ef24a95c
Fikri Firdaus <fikri.firdaus26@gmail.com>
2026-02-06 14:47:36 +0700

    deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.GITHUB_PAGES === "true" ? "/memo-game" : "",
+  assetPrefix: process.env.GITHUB_PAGES === "true" ? "/memo-game/" : "",
 };
 
 export default nextConfig;
