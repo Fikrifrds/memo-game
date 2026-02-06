1099ca3e674630d1c53e0f8fb7ad133282e21d72
Fikri Firdaus <fikri.firdaus26@gmail.com>
2026-02-06 15:43:15 +0700

    put on subdomain flipmatch.mlola.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.GITHUB_PAGES === "true" ? "/memo-game" : "",
-  assetPrefix: process.env.GITHUB_PAGES === "true" ? "/memo-game/" : "",
 };
 
 export default nextConfig;
